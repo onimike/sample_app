e1323aaf16785d11ff7889404341a3b715a1b33b
onimike <onimike@csu.fullerton.edu>
2012-03-12 17:32:03 -0700

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 SampleApp::Application.routes.draw do
-  get "pages/home"
-  get "pages/contact"
-  get "pages/about"
+  get "users/new"
+
+  match '/signup',  :to => 'users#new'
+
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
+
+  root :to => 'pages#home'
 end
