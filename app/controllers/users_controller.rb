c7608f70ca2428db2cd99b1bf9c4805aa4c6e28a
onimike <onimike@csu.fullerton.edu>
2012-03-30 01:24:21 -0700

    Chapter 8 done, user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def new
+	  @user = User.new
 	  @title = "Sign up"
   end
 
@@ -7,5 +8,15 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
 	@title = @user.name
   end
-
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      @title = "Sign up"
+      render 'new'
+    end
+  end
 end
